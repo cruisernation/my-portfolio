bb6f85405dd007a873e4edc46a023dc6f51d4dd9
cruisernation <136331510+cruisernation@users.noreply.github.com>
2025-12-05 13:50:20 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ export default function App(){
       <Header />
       <main className="container">
         <Routes>
-          <Route path='/' element={<Home/>} />
+          <Route path='/home' element={<Home/>} />
           <Route path='/about' element={<About/>} />
           <Route path='/services' element={<Services/>} />
           <Route path='/portfolio' element={<Portfolio/>} />
