3bdff0707b8667060ea68306d71dc293d2c271f2
cruisernation <136331510+cruisernation@users.noreply.github.com>
2025-12-28 11:02:18 +0100

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -34,7 +34,7 @@ export default function Home() {
       <div className="hero">
         <div className="hero-left">
           <h1 className="hero-title">
-            Hi, I am Abdulhamid;
+            Hi, I am Abdulhamid <br />
             <span className="typing">{text}</span>
           </h1>
 
