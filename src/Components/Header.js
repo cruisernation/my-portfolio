528203bd89d9c0e527220f9d956fcd7b30fd481b
cruisernation <136331510+cruisernation@users.noreply.github.com>
2025-12-30 17:07:12 +0100

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "./index.css";
+import "./header.css";
 
 export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
